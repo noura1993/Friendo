6e775a82726a09cd65a2231310cad5f37bab8b7d
Jack Morris <morriseo94@gmail.com>
2020-08-17 10:36:03 +0100

    login and back button in LogIn.js

--- friendo_client/components/LogIn.js
+++ friendo_client/components/LogIn.js
@@ -1,35 +1,45 @@
-import React, { Component } from 'react';
-import { View, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
 import t from 'tcomb-form-native';
-
+import FriendoButton from './FriendoButton';
 
 const Form = t.form.Form;
 
 const User = t.struct({
-    username: t.String,
-    password: t.String,
+  username: t.String,
+  password: t.String,
 });
 
 class LogIn extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form ref={(c) => (this._form = c)} type={User} />
+        <FriendoButton
+          text="Log In!"
+          buttonExternalStyles={styles.buttonExtraStyle}
+          onPressMethod={this.handleSubmit}
+        />
 
-    render() {
-        return (
-          <View style={styles.container}>
-            <Form 
-              ref={c => this._form = c}
-              type={User} 
-            />
-          </View>
-        )
-    }
+        <FriendoButton
+          text="Back"
+          buttonExternalStyles={styles.buttonExtraStyle}
+          onPressMethod={this.handleSubmit}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      justifyContent: 'center',
-      marginTop: 50,
-      padding: 20
-    },
-  });
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+  },
+  buttonExtraStyle: {
+    width: "100%"
+}
+});
 
 export default LogIn;
