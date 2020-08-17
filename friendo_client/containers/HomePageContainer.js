15bbd97ad16fd1793c12c017f7170aee09a4e2ff
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-17 23:23:05 +0100

    Add Home to Tab Navigator

--- friendo_client/containers/HomePageContainer.js
+++ friendo_client/containers/HomePageContainer.js
@@ -2,24 +2,9 @@ import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../components/Home';
 
-function HomeScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    );
-  }
-  
-  function SettingsScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-
-  const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 class HomePageContainer extends Component {
   constructor(props) {
@@ -36,8 +21,7 @@ class HomePageContainer extends Component {
     return (
        <NavigationContainer independent={true}>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
+                <Tab.Screen name="Home" component={Home} />
             </Tab.Navigator>
         </NavigationContainer>
     );
