ea3ec7cb2aec8ec869e20022a2c5bcf19a433d7d
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-18 00:40:28 +0100

    Remove unused import

--- friendo_client/containers/HomePageContainer.js
+++ friendo_client/containers/HomePageContainer.js
@@ -5,7 +5,6 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../components/Home';
 import Search from '../components/Search';
-import Profile from '../components/Profile';
 import Friends from '../components/Friends';
 
 const Tab = createBottomTabNavigator();
