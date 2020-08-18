7ff5d6ca8b5c2f911a9f0dff1c72efa550279be2
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-18 11:24:27 +0100

    Add AnimatedMap o Home

--- friendo_client/components/Home.js
+++ friendo_client/components/Home.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import MapView, { AnimatedRegion, Animated } from 'react-native-maps';
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      region: null
+    }
+
+    this.getInitialState = this.getInitialState.bind(this);
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
  
+  getInitialState() {
+    return {
+      region: new AnimatedRegion({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }),
+    };
+  }
+
+  onRegionChange(region) {
+    this.state.region.setValue(region);
+  }
+
   render() {
     return (
-        <View style={styles.home}>
-            <Text>Hello!</Text>
-        </View>
+      <MapView style={styles.home}> 
+        <Animated
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+       />
+      </MapView>
     );
   }
 }
