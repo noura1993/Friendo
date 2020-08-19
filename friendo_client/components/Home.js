1a5be7a3f10b574d4c754d3f6a3d5bcfaf592ff2
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-19 01:33:34 +0100

    Add bottom scroll view to home page

--- friendo_client/components/Home.js
+++ friendo_client/components/Home.js
@@ -1,6 +1,11 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity} from 'react-native';
-import MapView, { AnimatedRegion, Animated, Marker} from 'react-native-maps';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, TextInput, ScrollView, TouchableOpacity, Dimensions, Animated } from 'react-native';
+import MapView, { Marker, Animated as AnimatedRegion } from 'react-native-maps';
+
+
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = height * 0.25;
+const CARD_WIDTH = width * 0.6;
 
 class Home extends Component {
   constructor(props) {
@@ -14,37 +19,75 @@ class Home extends Component {
       },
       markers: [{
         coordinate: {
-          latitude: 55.953251,
+          latitude: 55.943251,
           longitude: -3.188267
         },
         image: require("../assets/FriendoLogo2.png"),
         name: "Jack Friendo",
-        age: "25-35",
+        age: "95+",
         description: "I have the best mustache :p"
+      },
+      {
+        coordinate: {
+          latitude: 55.963251,
+          longitude: -3.158267
+        },
+        image: require("../assets/FriendoLogo2.png"),
+        name: "Craig Friendo",
+        age: "95+",
+        description: "I have the best Guitar :p"
+      },
+      {
+        coordinate: {
+          latitude: 55.973251,
+          longitude: -3.168267
+        },
+        image: require("../assets/FriendoLogo2.png"),
+        name: "Noura Friendo",
+        age: "95+",
+        description: "I have the best baloon :p"
+      },
+      {
+        coordinate: {
+          latitude: 55.963251,
+          longitude: -3.188267
+        },
+        image: require("../assets/FriendoLogo2.png"),
+        name: "Rumen Friendo",
+        age: "95+",
+        description: "I have the best beard :p"
+      },
+      {
+        coordinate: {
+          latitude: 55.953251,
+          longitude: -3.209267
+        },
+        image: require("../assets/FriendoLogo2.png"),
+        name: "Ed Friendo",
+        age: "95+",
+        description: "I have the best eyeglasses :p"
       }],
       icons: {
-        logo:{
+        logo: {
           uri: require("../assets/FriendoLogo2.png")
         },
         searchIcon: {
           uri: require("../assets/search.png")
         },
-       
       },
-      
-      categories:[
-        {interest: 'Gaming', uri: require('../assets/gamepad.png')},
-        {interest: 'Sport',  uri: require('../assets/football.png')},
-        {interest: 'Art', uri: require('../assets/gallery.png')},
-        {interest: 'Gaming', uri: require('../assets/gamepad.png')},
-        {interest: 'Sport', uri: require('../assets/football.png')},
-        {interest: 'Art', uri: require('../assets/gallery.png')},
+      categories: [
+        { interest: 'Gaming', uri: require('../assets/gamepad.png') },
+        { interest: 'Sport', uri: require('../assets/football.png') },
+        { interest: 'Art', uri: require('../assets/gallery.png') },
+        { interest: 'Gaming', uri: require('../assets/gamepad.png') },
+        { interest: 'Sport', uri: require('../assets/football.png') },
+        { interest: 'Art', uri: require('../assets/gallery.png') },
       ]
     }
 
     this.onRegionChange = this.onRegionChange.bind(this);
   }
- 
+
   onRegionChange(region) {
     this.state.region.setValue(region);
   }
@@ -53,104 +96,134 @@ class Home extends Component {
   render() {
     return (
       <>
-      <MapView 
-        style={styles.home}
-        region={this.state.region}
-      > 
-      {this.state.markers.map( (marker) => (
-        <Marker coordinate={marker.coordinate}>
-          <Image source={marker.image} style={{height: 50, width:50 }} />
-        </Marker>
-      ))}
-      <Animated
-        region={this.state.region}
-        onRegionChange={this.onRegionChange}
-       />
-      </MapView>
-       <View style={styles.searchBar}>
-         
-       <TextInput 
-         placeholder="Search here"
-         placeholderTextColor="#000"
-         autoCapitalize="none"
-         style={{flex:1,padding:0}}
-       />
-        <Image source={this.state.icons.searchIcon.uri} style={{height: 25, width:25 }} />
-     </View>
-     <ScrollView
-        horizontal
-        scrollEventThrottle={1}
-        showsHorizontalScrollIndicator={false}
-        height={50}
-        style={styles.tagsScrollView}
-        contentInset={{ 
-          top:0,
-          left:0,
-          bottom:0,
-          right:20
-        }}
+        <MapView
+          style={styles.home}
+          region={this.state.region}
+        >
+          {this.state.markers.map((marker, index) => (
+            <Marker coordinate={marker.coordinate} key={index}>
+              <Image source={marker.image} style={{ height: 50, width: 50 }} />
+            </Marker>
+          ))}
+          <AnimatedRegion
+            region={this.state.region}
+            onRegionChange={this.onRegionChange}
+          />
+        </MapView>
+        <View style={styles.searchBar}>
+          <TextInput
+            placeholder="Search here"
+            placeholderTextColor="#000"
+            autoCapitalize="none"
+            style={{ flex: 1, padding: 0 }}
+          />
+          <Image source={this.state.icons.searchIcon.uri} style={{ height: 25, width: 25 }} />
+        </View>
+        <ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          style={styles.tagsScrollView}
+          contentInset={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 20
+          }}
         >
           {this.state.categories.map((category, index) => (
-          <TouchableOpacity key={index} style={styles.tagsItem}>
-            <Image style={styles.iconStyle} source={category.uri}/>
-          <Text>{category.interest}</Text>
-          </TouchableOpacity>
-        ))}
+            <TouchableOpacity key={index} style={styles.tagsItem}>
+              <Image style={styles.iconStyle} source={category.uri} />
+              <Text>{category.interest}</Text>
+            </TouchableOpacity>
+          ))}
         </ScrollView>
-     </>
+        <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          style={styles.bottomScrollView}>
+          {this.state.markers.map((marker, index) => (
+            <View style={styles.card} key={index}>
+              <Image
+                source={marker.image}
+                style={styles.cardImage}
+                resizeMode="cover"
+              />
+              <View style={styles.textContent}>
+                <Text>{marker.name}</Text>
+                <Text>{marker.age}</Text>
+                <Text>{marker.description}</Text>
+              </View>
+            </View>
+          ))}
+        </Animated.ScrollView>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    home: {
+  home: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }, 
+  },
   searchBar: {
-    position:'absolute', 
-    marginTop: Platform.OS === 'ios' ? 40 : 20, 
-    flexDirection:"row",
+    position: 'absolute',
+    marginTop: Platform.OS === 'ios' ? 40 : 20,
+    flexDirection: "row",
     backgroundColor: '#fff',
     width: '90%',
-    alignSelf:'center',
+    alignSelf: 'center',
     borderRadius: 5,
-    padding: 7,
-    shadowColor: '#ccc',
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    elevation: 10
+    padding: 7
   },
   tagsScrollView: {
-    position:'absolute', 
-    top:90, 
-    paddingHorizontal:10
+    position: 'absolute',
+    top: 90,
+    paddingHorizontal: 10
+  },
+  bottomScrollView: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingHorizontal: 10
   },
   tagsItem: {
-    flexDirection:"row",
-    backgroundColor:'#fff', 
-    borderRadius:20,
-    padding:8,
-    paddingHorizontal:20, 
-    marginHorizontal:5,
-    marginVertical: 5,
-    height:35,
-    shadowColor: '#ccc',
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    elevation: 10
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 8,
+    paddingHorizontal: 20,
+    marginHorizontal: 5,
+    height: 35
   },
   tagsIcon: {
     marginRight: 5,
   },
   iconStyle: {
     width: 18,
-    height:18,
+    height: 18,
     marginRight: 5
-
+  },
+  card: {
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    marginHorizontal: 10,
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH
+  },
+  cardImage: {
+    width: "60%",
+    height: "60%",
+    alignSelf: "center"
+  },
+  textContent: {
+    flex: 2,
+    padding: 10
   }
 });
 export default Home;
