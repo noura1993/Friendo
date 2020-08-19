d862c52a630532173bcf2568a5fcb6d8839ea9a6
Rumen Roger <rumenroger@gmail.com>
2020-08-19 12:04:23 +0100

    Merging Develop w/ Back & Fronends. Conflict fixing.

--- friendo_client/App.js
+++ friendo_client/App.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState, Fragment } from 'react';
-import { Text, View, ActivityIndicator, FlatList, Image, StyleSheet, Animated } from 'react-native';
+import { Text, View, ActivityIndicator, FlatList, Image, StyleSheet, Button } from 'react-native';
 import { ApiUrl } from './ApiUrl';
+import WelcomePageContainer from './containers/WelcomePageContainer';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import HomePageContainer from './containers/HomePageContainer';
+import Profile from './components/Profile';
+
+const Stack = createStackNavigator();
 
 const Friendo = () => {
   const [isLoading, setLoading] = useState(true);
@@ -31,77 +41,26 @@ const Friendo = () => {
   });
 
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello Bambino</Text>
-      {
-        isLoading ?
-          <ActivityIndicator />
-          : (
-            <Fragment>
-              <FlatList
-                data={usersList}
-                keyExtractor={({ id }, index) => id.toString()}
-                renderItem={({ item }) => (
-                  <Fragment>
-
-                    <Text>{item.firstname}</Text>
-
-                    <Image
-                      style={styles.tinyLogo}
-                      source={{
-                        uri: item.picture
-                      }}
-                    />
-
-                  </Fragment>
-                )}
-              />
-
-            </Fragment>
-          )
-      }
-    </View>
+  return ( 
+    <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator initialRouteName="Friendo" >
+        <Stack.Screen name="Friendo" component={WelcomePageContainer} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="SignUp" component={SignUp} /> 
+        <Stack.Screen name="Profile" component={Profile} /> 
+        <Stack.Screen name="Home" component={HomePageContainer} 
+          options={({ navigation }) => ({ 
+            headerLeft: null,
+            headerRight: () => (
+              <Button
+                onPress={() => navigation.navigate('Profile')}
+                title="Profile"
+                color="#00cc00" /> 
+          )})}
+        /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   )
-import React from 'react';
-import WelcomePageContainer from './containers/WelcomePageContainer';
-import LogIn from './components/LogIn';
-import SignUp from './components/SignUp';
-import { Button } from 'react-native';
-import { NavigationContainer, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
-import HomePageContainer from './containers/HomePageContainer';
-import Profile from './components/Profile';
-
-const Stack = createStackNavigator();
-
-const Friendo = () => {
-    return ( 
-      <NavigationContainer theme={DarkTheme}>
-        <Stack.Navigator initialRouteName="Friendo" >
-          <Stack.Screen name="Friendo" component={WelcomePageContainer} />
-          <Stack.Screen name="LogIn" component={LogIn} />
-          <Stack.Screen name="SignUp" component={SignUp} /> 
-          <Stack.Screen name="Profile" component={Profile} /> 
-          <Stack.Screen name="Home" component={HomePageContainer} 
-            options={({ navigation }) => ({ 
-              headerLeft: null,
-              headerRight: () => (
-                <Button
-                  onPress={() => navigation.navigate('Profile')}
-                  title="Profile"
-                  color="#00cc00" /> 
-            )})}
-          /> 
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
 }
 
 export default Friendo;
