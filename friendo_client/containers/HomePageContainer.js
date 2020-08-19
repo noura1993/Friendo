c0fccd16e3fc43569a1f143c7550e21d35b0faad
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-19 02:26:42 +0100

    remove unused files

--- friendo_client/containers/HomePageContainer.js
+++ friendo_client/containers/HomePageContainer.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../components/Home';
-import Search from '../components/Search';
 import Friends from '../components/Friends';
 
 const Tab = createBottomTabNavigator();
 
 const HomePageContainer = () => {
-    return (
-       <NavigationContainer independent={true}>
-          <Tab.Navigator initialRouteName="Home" >
-            <Tab.Screen name="Search" component={Search} />
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Friends" component={Friends} />
-          </Tab.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator initialRouteName="Home" >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Friends" component={Friends} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default HomePageContainer;
