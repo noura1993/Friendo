ba7bb17328a24fa28231dc1029a77f4aa9bf8853
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-20 17:07:42 +0100

    Add remove friend logic

--- friendo_client/components/Friends.js
+++ friendo_client/components/Friends.js
@@ -15,6 +15,7 @@ class Friends extends Component {
 
     this.updateFriends = this.updateFriends.bind(this);
     this.startChat = this.startChat.bind(this);
+    this.removeFriend = this.removeFriend.bind(this);
   }
 
   componentDidMount() {
@@ -27,6 +28,7 @@ class Friends extends Component {
       .then((json) => {
         const friends = json.map((friend) => {
           return {
+            friendshipId: friend.id,
             friendIcon: friend.friend_picture,
             friendName: friend.friend_first_name + ' ' + friend.friend_last_name,
             friendEmail: friend.friend_email,
@@ -47,15 +49,25 @@ class Friends extends Component {
     });
   }
 
+  removeFriend(friendshipId) {
+    fetch(ApiUrl('friends/' + friendshipId), {
+      method: 'DELETE'
+  }).then(() => this.updateFriends())
+    .catch((error) => console.error(error))
+  }
+
   render() {
     return (
       <View style={styles.friends}>
-        {this.state.friends.map( (friend) => (
-          <View>
+        {this.state.friends.map( (friend, index) => (
+          <View key={index}>
             <Image source={friend.friendIcon} style={{ height: 10, width: 10 }} />
             <Text>{friend.friendName}</Text>
             <TouchableOpacity onPress={() => this.startChat(friend.friendEmail, friend.friendName)}>
               <Text>Chat!</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.removeFriend(friend.friendshipId)}>
+              <Text>Remove!</Text>
             </TouchableOpacity>
           </View>
         ))}
